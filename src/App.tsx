fbecba4c9a7aab8c0a2a0836cd5eec0704efde59
jingoworld <lonul83@gmail.com>
2023-02-24 11:37:20 +0900

    chore: 리액트 쿼리 관리자 도구 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import A from '@/components/A';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ const App = () => {
         Spam Mayo Frontend
         <A />
         <button onClick={onClick}>BTN</button>
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </div>
   );
