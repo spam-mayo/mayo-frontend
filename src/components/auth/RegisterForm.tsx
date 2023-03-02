dcdc36076ec643413235ec51790c1024d4c445b6
jjeonge97 <cowguswjd@gmail.com>
2023-03-02 14:09:00 +0900

    refactor:에러 메세지 추가

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -6,6 +6,15 @@ import { Select, type SelectOption } from './Select';
 import './registerForm.scss';
 import { type RegisterSchema, registerSchema } from '@/constants/schema/registerSchema';
 
+const categoryOption: SelectOption[] = [
+  { label: '선택 안 함', value: 'nofield', id: 1 },
+  { label: '프론트엔드', value: 'frontend', id: 2 },
+  { label: '백엔드', value: 'backend', id: 3 },
+  { label: '디자인', value: 'design', id: 4 },
+  { label: '기획', value: 'plan', id: 5 },
+  { label: '기타', value: 'other', id: 6 },
+];
+
 export const RegisterForm: FC = () => {
   const {
     handleSubmit,
@@ -15,15 +24,6 @@ export const RegisterForm: FC = () => {
   } = useForm<RegisterSchema>({
     resolver: yupResolver(registerSchema),
   });
-
-  const categoryOption: SelectOption[] = [
-    { label: '선택 안 함', value: 'nofield', id: 1 },
-    { label: '프론트엔드', value: 'frontend', id: 2 },
-    { label: '백엔드', value: 'backend', id: 3 },
-    { label: '디자인', value: 'design', id: 4 },
-    { label: '기획', value: 'plan', id: 5 },
-    { label: '기타', value: 'other', id: 6 },
-  ];
 
   const [isEmailChecked, setIsEmailChecked] = useState(false);
 
--- src/constants/schema/loginSchema.ts
+++ src/constants/schema/loginSchema.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 
 export const loginSchema = yup
   .object({
-    email: yup.string().email().required('이메일을 입력해주세요.'),
+    email: yup.string().email('올바른 이베일 형식이 아닙니다.').required('이메일을 입력해주세요.'),
     password: yup
       .string()
       .required('비밀번호를 입력해주세요')
