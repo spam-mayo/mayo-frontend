a2d1efa1b9fd047e1082596526bb8f44b8c14f42
jjeonge97 <cowguswjd@gmail.com>
2023-03-06 13:35:59 +0900

    refactor:상대경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { PasswordChange } from './pages/auth/PasswordChange';
-import { Login } from './pages/auth/Login';
-import { Register } from './pages/auth/Register';
-import Main from './pages/Main';
+import { PasswordChange } from '@/pages/auth/PasswordChange';
+import { Login } from '@/pages/auth/Login';
+import { Register } from '@/pages/auth/Register';
+import Main from '@/pages/Main';
 
 const queryClient = new QueryClient();
 
