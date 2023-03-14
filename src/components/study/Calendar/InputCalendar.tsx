93137c6ff4bc84124c8ebc779ecf8b916b577f9c
jingoworld <lonul83@gmail.com>
2023-03-14 21:06:01 +0900

    style: 달력 라이브러리 업데이트

---
+++ src/components/study/Calendar/InputCalendar.tsx
@@ -0,0 +1,18 @@
+import CalendarRange from '@/components/common/Calendar';
+
+const InputCalendar = () => {
+  return (
+    <div className="input-calendar-container">
+      <label>모임기간</label>
+      <div className="input-wrapper">
+        <i className="icon-calendar" />
+        <input placeholder="모임기간을 선택하세요" type="text" />
+      </div>
+      <div className="calendar-wrapper">
+        <CalendarRange />
+      </div>
+    </div>
+  );
+};
+
+export default InputCalendar;
