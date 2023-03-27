75b8c2a110548bdbec9f385ec645a548351ad608
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-27 11:03:24 +0900

    [Feat] 회원 추가 정보 저장 로직 (#50)
    * feat:추가 정보 API 연동
    
    * feat:기존 활동분야 defaulValue 설정
    
    * refactor:활동분야 미선택시 field 항목 삭제
    
    * feat:회원 추가정보 수정 API 연동
    
    * refactor:관심분야 데이터 받기
    
    * refactor: name default value
    
    * refactor:Stack 코드정리
    
    * fix: 요청사항 반영

--- src/components/mypage/UserInfo/ExtraInfo.tsx
+++ src/components/mypage/UserInfo/ExtraInfo.tsx
@@ -1,23 +1,76 @@
 import MultiButton from '@/components/mypage/UserInfo/MultiButton';
-import { type FC, useState } from 'react';
+import { type FC, useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import Select from '@/components/auth/Select';
-import { categoryOption } from '@/constants/categoryOption';
+import { fieldOption } from '@/constants/fieldOption';
 import type { Stack } from '@/api/auth/types';
+import { useMutation } from '@tanstack/react-query';
+import { patchUserInfo } from '@/api/auth/authAPI';
+import axios from 'axios';
+import StackForm from '@/components/study/Stack';
 
 interface Props {
   field: string;
   stack?: Stack[];
+  userId: string;
 }
 
-const ExtraInfo: FC<Props> = ({ field, stack = [] }) => {
-  const [isEdit, setIsEdit] = useState(false);
+const tmp = (categoryLabel: string) => {
+  const res = fieldOption.find(({ label }) => label === categoryLabel);
+  return res;
+};
 
-  const onClickEdit = () => {
-    setIsEdit(!isEdit);
+const ExtraInfo: FC<Props> = ({ field, stack = [], userId }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [checkedList, setCheckedList] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const { mutate: patchToUserInfo } = useMutation(patchUserInfo, {
+    onSuccess: () => {
+      alert('추가 정보가 저장되었습니다!');
+      setIsEdit(false);
+    },
+    onError: (err) => {
+      if (axios.isAxiosError(err)) {
+        if (err.response?.status === 404) {
+          alert('일치하는 정보가 없습니다.');
+        }
+        if (err.response?.status === 400) {
+          alert('변경할 항목을 선택해주세요.');
+        }
+      }
+    },
+  });
+
+  const onChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCategory(event.target.value);
   };
 
+  const onChangeCheckList = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setCheckedList((prev) => (prev.includes(value) ? prev.filter((p) => p === value) : [...prev, value]));
+  };
+
+  const onClickEdit = () => {
+    setIsEdit((prev) => !prev);
+    setCheckedList(stack.map(({ stackName }) => stackName));
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    patchToUserInfo({
+      userId,
+      userStacks: checkedList,
+      field: selectedCategory || undefined,
+    });
+  };
+
+  useEffect(() => {
+    setSelectedCategory(tmp(field)?.value ?? '');
+  }, [field]);
+
   return (
-    <div className="userInfo-container">
+    <form className="userInfo-container" onSubmit={onSubmit}>
       <div className="userInfo-container-top">
         <p>추가 정보</p>
         <MultiButton onClick={onClickEdit} isEdit={isEdit} />
@@ -26,21 +79,22 @@ const ExtraInfo: FC<Props> = ({ field, stack = [] }) => {
         {isEdit ? (
           <>
             <div className="row">
-              <p className="key">활동 분야</p>
-              <Select options={categoryOption} />
+              <p className="label">활동 분야</p>
+              <Select options={fieldOption} value={selectedCategory} onChange={onChangeSelect} />
             </div>
             <div className="row">
-              <p className="key">관심 분야</p>
+              <p className="label">관심 분야</p>
+              <StackForm onChange={onChangeCheckList} checked={checkedList} />
             </div>
           </>
         ) : (
           <>
             <div className="row">
-              <p className="key">활동 분야</p>
+              <p className="label">활동 분야</p>
               <p className="value">{field}</p>
             </div>
             <div className="row">
-              <p className="key">관심 분야</p>
+              <p className="label">관심 분야</p>
               <div className="stack-box">
                 {stack ? (
                   stack.map((el, idx) => {
@@ -54,7 +108,7 @@ const ExtraInfo: FC<Props> = ({ field, stack = [] }) => {
           </>
         )}
       </div>
-    </div>
+    </form>
   );
 };
 
