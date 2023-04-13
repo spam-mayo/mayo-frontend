1142ccf5f9edf80cde1df9021dba9a7f5dc1e45f
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-04-13 17:55:41 +0900

    [Feat] 신청 스터디 조회 (#57)
    * feat:신청 스터디 API 연동
    
    * feat:옵션 선택 시 1페이지로 이동
    
    * refactor:useEffect 제거
    
    * feat:스터디 신청 현황에 따라 분류
    
    * fix::data.length 오류 수정

--- src/constants/mypageOption.ts
+++ src/constants/mypageOption.ts
@@ -8,3 +8,10 @@ export const mystudyOption: SelectOption[] = [
   { label: '폐쇄', value: 'closed' },
   { label: '종료', value: 'end' },
 ];
+
+export const applyStudyOption: SelectOption[] = [
+  { label: '전체', value: 'all' },
+  { label: '대기중', value: 'waiting' },
+  { label: '승인', value: 'approval' },
+  { label: '거절', value: 'reject' },
+];
