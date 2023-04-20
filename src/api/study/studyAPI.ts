069e92aceaa236cd4b386814fca7362ea71010de
jjeonge97 <cowguswjd@gmail.com>
2023-04-21 08:55:48 +0900

    fix:studyAPI 파일명 변경

--- src/api/study/studyAPI.ts
+++ src/api/study/studyAPI.ts
@@ -1,6 +1,8 @@
 import axiosInstance from '@/api/axiosInstance';
-import type { GetMyStudyRes, PaginationRes } from '@/api/study/studyTypes';
+import type { GetMyStudyRes, GetStudyDetailRes, PaginationRes } from '@/api/study/studyTypes';
 
 export const getMypageStudy = (page: number, params?: { status?: string; tab?: string; approvalStatus?: string }) => {
   return axiosInstance.get<PaginationRes<GetMyStudyRes>>('/api/study/my-page?size=8', { params: { page, ...params } });
 };
+
+export const getStudyDetail = (studyId: number) => axiosInstance.get<GetStudyDetailRes>(`/api/study/${studyId}`);
