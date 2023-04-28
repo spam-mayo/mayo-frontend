84ba156e48718d267659f9d3817020933942fc4f
jjeonge97 <cowguswjd@gmail.com>
2023-04-28 09:39:47 +0900

    feat:고정 공지사항 추가

--- src/components/study/studySchedule/StudySchedule.tsx
+++ src/components/study/studySchedule/StudySchedule.tsx
@@ -1,7 +1,12 @@
+import Announcement from '@/components/study/studySchedule/Announcement';
 import type { FC } from 'react';
 
 const StudySchedule: FC = () => {
-  return <div>스터디 일정 컴포넌트</div>;
+  return (
+    <div>
+      <Announcement />
+    </div>
+  );
 };
 
 export default StudySchedule;
