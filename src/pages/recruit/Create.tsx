6fac3fa914ea82f26b16c368ba713ed8da8eed5e
jjeonge97 <cowguswjd@gmail.com>
2023-05-30 21:00:15 +0900

    feat:구인글 생성 API 연동

--- src/pages/recruit/Create.tsx
+++ src/pages/recruit/Create.tsx
@@ -1,11 +1,17 @@
+import { postRecruit } from '@/api/recruit/recruitAPI';
+import type { PostRecruitReq } from '@/api/recruit/recruitTypes';
 import { getStudyDetail } from '@/api/study/studyAPI';
 import Button from '@/components/common/Button';
 import StudyDetailIntro from '@/components/common/StudyDetailIntro';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import type { FC } from 'react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 const RecruitCreate: FC = () => {
   const studyId = 266;
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<PostRecruitReq>();
 
   const { data } = useQuery({
     queryFn: () => getStudyDetail(Number(studyId)),
@@ -13,12 +19,27 @@ const RecruitCreate: FC = () => {
     select: ({ data }) => data,
   });
 
+  const { mutate: postNewRecruit } = useMutation(postRecruit, {
+    onSuccess: () => {
+      alert('구인글이 작성되었습니다!');
+      navigate('/');
+    },
+  });
+
+  const onClickGoBack = () => {
+    navigate(-1);
+  };
+
+  const onSumbit: SubmitHandler<PostRecruitReq> = (data) => {
+    postNewRecruit({ studyId, body: data });
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-lg-12">
           <div className="title-area">
-            <button>
+            <button onClick={onClickGoBack}>
               <i className="icon-arrow-left"></i>
             </button>
             <p>구인 글 생성하기</p>
@@ -26,15 +47,15 @@ const RecruitCreate: FC = () => {
           </div>
           <StudyDetailIntro detailData={data} />
 
-          <div className="add-recruit-box">
+          <form className="add-recruit-box" onSubmit={handleSubmit(onSumbit)} id="recruit">
             <div className="recruit-intro">
               <p>스터디 소개</p>
-              <textarea placeholder="내용을 작성해주세요." required name="recruit-intro" />
+              <textarea placeholder="내용을 작성해주세요." required form="recruit" {...register('offerIntro')} />
             </div>
 
             <div className="recruit-rule">
               <p>스터디 규칙</p>
-              <textarea placeholder="내용을 작성해주세요." required name="recruit-intro" />
+              <textarea placeholder="내용을 작성해주세요." required form="recruit" {...register('offerRule')} />
             </div>
             <div className="button-area">
               <Button size="large" color="gray" outline>
@@ -44,7 +65,7 @@ const RecruitCreate: FC = () => {
                 생성하기
               </Button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
