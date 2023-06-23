1e9a4cf8e25f6aba85275295ca1a660aeddd71ed
jjeonge97 <cowguswjd@gmail.com>
2023-06-23 13:17:41 +0900

    feat:공백 입력 시 알림창 노출

--- src/queries/study/usePostStudyCommentMutation.ts
+++ src/queries/study/usePostStudyCommentMutation.ts
@@ -1,10 +1,16 @@
 import { postStudyComment } from '@/api/study/studyAPI';
 import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 const usePostStudyCommentMutation = () => {
   return useMutation(postStudyComment, {
     onSuccess: () => {
       alert('등록되었습니다!');
+    },
+    onError: (err) => {
+      if (axios.isAxiosError(err)) {
+        if (err.response?.status === 400) alert('댓글이 공백일 수 없습니다.');
+      }
     },
   });
 };
