cade429315f4e5641377eaa4a8d59a0b5dd93e3a
jjeonge97 <cowguswjd@gmail.com>
2023-06-26 16:19:54 +0900

    feat:유효성 검사 추가

--- src/components/modal/NoticeEditModal.tsx
+++ src/components/modal/NoticeEditModal.tsx
@@ -1,8 +1,10 @@
 import type { PatchNoticeReq } from '@/api/study/studyTypes';
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
+import { noticeSchma } from '@/constants/schema/noticeSchema';
 import useNoticeQuery from '@/queries/study/useNoticeQuery';
 import usePatchNoticeMutation from '@/queries/study/usePatchNoticeMutation';
+import { yupResolver } from '@hookform/resolvers/yup';
 import type { FC } from 'react';
 import { useForm } from 'react-hook-form';
 
@@ -24,7 +26,7 @@ const NoticeEditModal: FC<Props> = ({ onClose, studyId, isEdit }) => {
     handleSubmit,
     register,
     formState: { errors },
-  } = useForm<PatchNoticeReq>();
+  } = useForm<PatchNoticeReq>({ resolver: yupResolver(noticeSchma) });
 
   const onSumbit = (data: PatchNoticeReq) => {
     patchNotice.mutate({ ...data, studyId: Number(studyId) });
@@ -36,18 +38,24 @@ const NoticeEditModal: FC<Props> = ({ onClose, studyId, isEdit }) => {
   return (
     <div className="modal-container">
       <form className="modal-content notice-modal" onSubmit={handleSubmit(onSumbit)}>
-        <p>공지사항 {isEdit ? '수정' : '등록'}</p>
-        <Input
-          placeholder="공지사항 제목을 입력해주세요."
-          {...register('noticeTitle')}
-          defaultValue={isEdit ? noticeTitle : ''}
-        />
-        <textarea
-          placeholder="공지사항 내용을 입력해주세요."
-          {...register('noticeContent')}
-          defaultValue={isEdit ? noticeContent : ''}
-        />
-        {errors.noticeContent && <p>{errors.noticeContent.message}</p>}
+        <p className="notice-title">공지사항 {isEdit ? '수정' : '등록'}</p>
+        <div>
+          <Input
+            placeholder="공지사항 제목을 입력해주세요."
+            {...register('noticeTitle')}
+            defaultValue={isEdit ? noticeTitle : ''}
+          />
+          {errors.noticeTitle && <p className="err-msg">{errors.noticeTitle.message}</p>}
+        </div>
+        <div>
+          {' '}
+          <textarea
+            placeholder="공지사항 내용을 입력해주세요."
+            {...register('noticeContent')}
+            defaultValue={isEdit ? noticeContent : ''}
+          />
+          {errors.noticeContent && <p className="err-msg">{errors.noticeContent.message}</p>}
+        </div>
         <div>
           <Button onClick={onClose} color="gray" outline>
             취소
